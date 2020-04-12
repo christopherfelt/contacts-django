b3bf7f2370f8557e96ce13b6f86416f787333413
chrisfelt <chrisfelt.webdev@gmail.com>
2020-04-12 14:01:18 -0600

    add git ignore and readme

--- contacts/settings.py
+++ contacts/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuapp.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
